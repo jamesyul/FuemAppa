b6973942c8797600bb5b6616528a7f6bbb0d4fd1
Yul Cardenas <69341405+jamesyul@users.noreply.github.com>
2025-02-20 13:24:50 +0100

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,6 @@
-/*import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './src/components/common/Navbar';
 import Home from './src/pages/Home';
 import Login from './src/pages/Login';
 import Pieces from './src/pages/Pieces';
@@ -34,11 +8,14 @@ import Pieces from './src/pages/Pieces';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/pieces" element={<Pieces />} />
-      </Routes>
+      <Navbar />
+      <div className="pt-16"> {/* Espacio para evitar que el contenido se solape con la navbar */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/pieces" element={<Pieces />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
